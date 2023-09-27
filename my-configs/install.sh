7cf12b96e234119ef7cbbc7ada500e5415bd3979
Kupro404 <145548360+Kupro404@users.noreply.github.com>
2023-09-27 08:32:03 +0300

    Update install.sh

--- my-configs/install.sh
+++ my-configs/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#useful tools
+apt install exa
+apt install bat 
+apt install fzf
+apt install vim 
+rm ~/.vimrc
+
 #dot files path
 mv .zshrc ~
 mv .vimrc ~
@@ -7,7 +14,3 @@ mv .tmux.conf ~
 rm /etc/zsh/zshrc
 mv zshrc /etc/zsh
 
-#useful tools
-apt install exa
-apt install bat 
-apt install fzf
